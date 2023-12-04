d6ce7f4d0ca69434985b599d90ee2f5431db1741
Anne Avery <anne@anneavery.org>
2023-12-03 20:09:07 -0800

    dpi adjustments + more stations

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let width, height;
+
 let mouseOrigin = {x: 0, y: 0};
 let mouseDown = false;
 let offset = {x: 123.1, y: -49.2};
@@ -11,6 +13,10 @@ canvas.onmousedown = event => {
 	mouseOrigin = {x: event.offsetX, y: event.offsetY};
 	offsetOrigin = {x: offset.x, y: offset.y};
 	mouseDown = true;
+}
+
+canvas.onscroll = event => {
+	console.log(event);
 }
 
 canvas.onmouseup = event => {
@@ -38,25 +44,33 @@ function render() {
 }
 
 function drawStop(x, y) {
-	console.log("draw stop at " + x + ", " + y);
-	ctx.fillStyle = "black";
-	ctx.fillRect(x, y, 10, 10);
+	ctx.strokeStyle = "black";
+	ctx.fillStyle = "white";
+	ctx.lineWidth = 2;
+	ctx.beginPath();
+	ctx.arc(x, y, 6, 0, 2*Math.PI);
+	ctx.fill();
+	ctx.stroke();
 }
 
 function latlongToCanvasX(x) {
-	return (x + offset.x) * scale + canvas.width/2;
+	return (x + offset.x) * scale + width/2;
 }
 
 function latlongToCanvasY(y) {
-	return (y + offset.y) * -scale + canvas.height/2;
+	return (y + offset.y) * -scale + height/2;
 }
 
-// function canvasToLatlongX(x) {
-// 	return (x - canvas.width/2) / scale + offset.x;
-// }
+let dpr = window.devicePixelRatio || 1;
+let bsr = ctx.backingStorePixelRatio || 1;
+let ratio = dpr/bsr;
+width = window.innerWidth;
+height = window.innerHeight;
 
-// function canvasToLatlongY(y) {
-// 	return (y - canvas.width/2) / -scale + offset.y;
-// }
+canvas.width = width * ratio;
+canvas.height = height * ratio;
+canvas.style.width = width + "px";
+canvas.style.height = height + "px";
+ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
 
 render();
